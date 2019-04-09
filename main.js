1248e625737dfb239b59b3320e24855326d5d2b4
Margarita <margomimi82@gmail.com>
2019-04-09 17:06:54 +0300

    change function

--- main.js
+++ main.js
@@ -5,18 +5,21 @@
 })()
 
 function getSimpleNumbers(){
-	var min = parseInt(prompt('Minimal value')),
-		max = parseInt(prompt('Maximal value'));
-	if(min>1){
-	Num:for (var i=min; i<=max; i++) {
-			for (var j = min; j < i; j++) {
+	var min = prompt('Minimal value'),
+		max = prompt('Maximal value');
+		if(min>1){
+	Num:for (var i = 2; i <= max; i++) {
+
+    for (var j = min; j < i; j++) {
       if ((i % j) == 0) continue Num;
     }
-    alert(i)
-		}
-	}
-else{
-	alert('false');
+
+    alert(i); // простое
+  }
+}else alert('enter minimal value over then 2')
 }
+/*function primeNum(n){
+
 }
+*/
 
